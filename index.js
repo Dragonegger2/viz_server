9789a8e3c267fa226212b688805b4d7352637d4c
Trent Lennon <tlennon@ebsco.com>
2016-06-14 11:22:10 -0400

    Now properly uploads the file to the server.

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 var express = require('express');
 var multer = require('multer');
-var upload = multer({dest: './uploads/' });
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, './uploads/')
+  },
+  filename: function (req, file, cb) {
+    console.log(file);
+    //cb(null, "test" + Date.now() + '.' + mime.extension(file.mimetype));
+    cb(null, file.originalname);
+  }
+});
+var upload = multer({ storage: storage });
 
 var app = express();
 
